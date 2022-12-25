e1c0bcc9ca78035104945eb8140f257a87a9119a
Сергей <tolltroll@yandex.ru>
2022-12-25 11:33:47 +0300

    Init commit

--- src/main/java/org/example/servlet/MainServlet.java
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -15,7 +15,7 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Hello dude!");
+        printWriter.write("Hello dude! How are ya?");
         printWriter.close();
     }
 }
